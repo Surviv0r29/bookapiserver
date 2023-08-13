6a12fd3166944e497447996c4b40624f3dff0322
Surviv0r29 <sshankha.123@gmail.com>
2023-08-13 21:05:19 +0530

    new pass

--- src/dto/create-book.dto.ts
+++ src/dto/create-book.dto.ts
@@ -1,23 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsInt, Min, Max } from 'class-validator';
 
 export class CreateBookDto {
+  @ApiProperty({ example: 'Harry Potter', description: 'The name of the book' })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({ example: 'Harry Potter is a.....', description: 'The description of the book' })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({ example:3, description: 'The discount of the book' })
   @IsInt()
   @Min(1)
   @Max(99)
   discountRate: number;
 
+  @ApiProperty({ example: 'image url', description: 'The url of the image of the book' })
   @IsNotEmpty()
   @IsString()
   coverImage: string;
 
+  @ApiProperty({ example: 33, description: 'price of the book' })
   @IsNotEmpty()
   @IsInt()
   price: number;
--- src/dto/delete-book.dto.ts
+++ src/dto/delete-book.dto.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty, IsInt} from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class DeleteBookDto {
+  @ApiProperty({ example: 1, description: 'id of the book' })
   @IsNotEmpty()
   @IsInt()
   id: number;
